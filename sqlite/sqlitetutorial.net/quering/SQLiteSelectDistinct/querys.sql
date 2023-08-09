d5925328c777307b5fdddaf0ad99d4f6e7efd9c7
Skaloshad <dalex90mail@gmail.com>
2023-08-09 17:52:05 +0300

    restructurize SQLite tutorial

--- sqlite/sqlitetutorial.net/quering/SQLiteSelectDistinct/querys.sql
+++ sqlite/sqlitetutorial.net/quering/SQLiteSelectDistinct/querys.sql
@@ -1,3 +1,33 @@
 -- database: ..\..\chinook.db
 
 -- Use the ▷ button in the top right corner to run the entire file.
+
+SELECT city
+FROM customers
+ORDER BY city;
+
+SELECT DISTINCT city
+FROM customers
+ORDER BY city;
+
+SELECT
+  city,
+  country
+FROM
+  customers
+ORDER BY
+  country;
+
+SELECT DISTINCT
+  city,
+  country
+FROM
+  customers
+ORDER BY
+  country;
+
+SELECT company
+FROM customers;
+
+SELECT DISTINCT company
+FROM customers;
