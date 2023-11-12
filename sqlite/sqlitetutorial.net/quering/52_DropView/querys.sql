18212cf521e922d5aa7606a4e6ddbeff6dfdcc6d
Skaloshad <dalex90mail@gmail.com>
2023-11-12 18:40:19 +0300

    Begin DROP VIEW

--- sqlite/sqlitetutorial.net/quering/52_DropView/querys.sql
+++ sqlite/sqlitetutorial.net/quering/52_DropView/querys.sql
@@ -1,3 +1,20 @@
 -- database: ..\..\chinook.db
 
 -- Use the ▷ button in the top right corner to run the entire file.
+
+CREATE VIEW v_billing (
+  invoiceId,
+  invoiceDate,
+  total
+)
+AS
+  SELECT
+    invoiceId,
+    invoiceDate,
+    SUM (unitPrice * quantity)
+  FROM
+    invoices
+  INNER JOIN
+    invoice_items USING (
+      invoiceId
+    );
