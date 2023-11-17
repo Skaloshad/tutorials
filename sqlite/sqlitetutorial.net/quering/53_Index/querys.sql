17aef183c0005a94c9937f02837abcc3c1efdc4e
Skaloshad <dalex90mail@gmail.com>
2023-11-17 10:37:54 +0300

    Add SQLite index continue

--- sqlite/sqlitetutorial.net/quering/53_Index/querys.sql
+++ sqlite/sqlitetutorial.net/quering/53_Index/querys.sql
@@ -1,3 +1,22 @@
 -- database: ..\..\chinook.db
 
 -- Use the ▷ button in the top right corner to run the entire file.
+
+CREATE TABLE contacts (
+  firstName TEXT NOT NULL,
+  lastName TEXT NOT NULL,
+  email TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_contacts_email
+ON contacts (email);
+
+INSERT INTO contacts (firstName, lastName, email)
+VALUES('John','Doe','john.doe@sqlitetutorial.net');
+
+INSERT INTO contacts (firstName, lastName, email)
+VALUES('Johny','Doe','john.doe@sqlitetutorial.net');
+
+INSERT INTO contacts (firstName, lastName, email)
+VALUES('David','Brown','david.brown@sqlitetutorial.net'),
+      ('Lisa','Smith','lisa.smith@sqlitetutorial.net');
